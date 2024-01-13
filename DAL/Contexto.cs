c1b0acc80b619aafc82e60ea08bf7e5c34db2db1
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2024-01-13 18:22:42 -0400

    Agregando El Contexto y Clase Prioridades

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroDePrioridadesMABB1.Models;
+
+namespace RegistroDePrioridadesMABB1.DAL
+{
+    public class Contexto:DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Prioridades> Prioridades { get; set; }
+    }
+}
