0436d03761c16b5f1e4a5549fd20832221e0f2f2
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2024-01-13 20:40:09 -0400

    Arreglando Errores

--- BLL/PrioridadesBLL.cs
+++ BLL/PrioridadesBLL.cs
@@ -8,27 +8,36 @@ namespace RegistroDePrioridadesMABB1.BLL
     public class PrioridadesBLL
     {
         private readonly Contexto _contexto;
-
         public PrioridadesBLL(Contexto contexto)
         {
             _contexto = contexto;
         }
+        public bool Existe(int PrioridadId)
+        {
+            return _contexto.Prioridades.Any(p => p.PrioridadId == PrioridadId);
 
-        public bool Existe(int prioridadID)
-        {
-            return _contexto.Prioridades.Any(p => p.PrioridadId == prioridadID);
         }
-
-        public bool Guardar(Prioridades prioridades)
+        public bool Guardar(Prioridades Prioridades)
         {
-            if (!Existe(prioridades.PrioridadId)) return this.Insertar(prioridades);
-            else return this.Modificar(prioridades);
+            if (!Existe(Prioridades.PrioridadId))
+                return this.Insertar(Prioridades);
+            else
+                return this.Modificar(Prioridades);
         }
-        public bool Eliminar(Prioridades Prioridades)
+        public bool Eliminar(int id)
         {
-            var p = _contexto.Prioridades.Find(Prioridades.PrioridadId);
-            _contexto.Entry(p!).State = EntityState.Detached;
-            _contexto.Entry(Prioridades).State = EntityState.Deleted;
+            var prioridades = _contexto.Prioridades.Find(id);
+            _contexto.Prioridades.Remove(prioridades);
+            var deleted = _contexto.SaveChanges() > 0;
+            return deleted;
+        }
+        public async Task<Prioridades?> Buscar(int id)
+        {
+            return await _contexto.Prioridades.FindAsync(id);
+        }
+        public bool Insertar(Prioridades Prioridades)
+        {
+            _contexto.Prioridades.Add(Prioridades);
             return _contexto.SaveChanges() > 0;
         }
         public bool Modificar(Prioridades Prioridades)
@@ -38,19 +47,10 @@ namespace RegistroDePrioridadesMABB1.BLL
             _contexto.Entry(Prioridades).State = EntityState.Modified;
             return _contexto.SaveChanges() > 0;
         }
-        public bool Insertar(Prioridades Prioridades)
+        public List<Prioridades> Getlist(Expression<Func<Prioridades, bool>> criterio)
         {
-            _contexto.Prioridades.Add(Prioridades);
-            return _contexto.SaveChanges() > 0;
+            return _contexto.Prioridades.Where(criterio).AsNoTracking().ToList();
         }
-        public async Task<Prioridades?> Buscar(int id)
-        {
-            return await _contexto.Prioridades.FindAsync(id);
-        
     }
-        public List<Prioridades> Getlist(Expression<Func<Prioridades, bool>> Criterio)
-        {
-            return _contexto.Prioridades.Where(Criterio).AsNoTracking().ToList();
-        }
-      }
-    }
+}
+
